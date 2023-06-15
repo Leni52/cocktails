3666055ebd0459cfcac45bc3a13ecc2288675918
Lenche Petrov <lenche.petrov@scalefocus.com>
2023-06-15 16:17:44 +0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About/>}/>
           <Route path="/cocktails" element={<Cocktail />} />
         </Routes>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import Cocktail from "./Cocktail";
-
+import React, { useState } from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
         <>
+        <div className="container">
+          
+          </div>
           <Cocktail></Cocktail>
         </>
     );
